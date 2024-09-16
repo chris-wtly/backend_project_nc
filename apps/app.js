7e98e1b5eaba47f34b9a7098d736704595c15fb5
chris-wtly <christianwhitley19@outlook.com>
2024-09-16 10:21:03 +0100

    allow cross origin resource sharing

--- apps/app.js
+++ apps/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const {
   getTopics,
@@ -12,6 +13,8 @@ const {
   deleteComment,
   getUsers,
 } = require("../controllers/controller");
+
+app.use(cors());
 
 app.use(express.json());
 
